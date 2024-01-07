48dc7b9ba5316d14c1cfd652bdd44e9d1256f009
EmilioGalimberti <emiliogalimberty@gmail.com>
2024-01-07 18:42:00 -0300

    DATA BINDING
    https://learn.udacity.com/courses/ud9012/lessons/9dc56bee-f369-4205-8910-6b370e157872/concepts/e4e1bf05-aa45-4bac-82f9-cb7f5dc761d5

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,24 +8,40 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        //findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        binding.doneButton.setOnClickListener { addNickname(it) }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+        //val editText = findViewById<EditText>(R.id.nickname_edit)
+        //val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
-
+        /*
+        binding.apply { ... }: Esto es una extensión de Kotlin llamada apply. La función apply toma un objeto y le permite ejecutar un bloque de código en el contexto de ese objeto.
+        En este caso, binding.apply se utiliza para aplicar una serie de operaciones en el objeto binding.
+         */
+        binding.apply {
+        binding.nicknameText.text = binding.nicknameEdit.text
+        /*
+        invalidateAll(): Esta función se usa para indicar al sistema de enlace de datos que debe invalidar todas las asignaciones y forzar una actualización de la interfaz de usuario.
+        En el contexto de Android Data Binding, invalidateAll() es comúnmente usado para notificar a la interfaz de usuario que los datos subyacentes han cambiado y que debe volver a evaluar
+        todas las expresiones de enlace.
+         */
+        invalidateAll()
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+        }
         //hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
